7d348468462a1cf52d5fea8020725a8664df3644
Teabx <cerandior@hotmail.com>
2020-03-13 15:45:34 +0100

    Pipe now updates its blockstate

--- src/main/java/teabx/vanillaextended/blocks/TransportPipe.java
+++ src/main/java/teabx/vanillaextended/blocks/TransportPipe.java
@@ -9,12 +9,15 @@ import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import teabx.vanillaextended.blocks.interfaces.IStorageBlockPart;
 import teabx.vanillaextended.tileentities.TPTile;
@@ -41,20 +44,19 @@ public class TransportPipe extends Block{
         .with(NORTH, Boolean.valueOf(false)).with(EAST, Boolean.valueOf(false)).with(SOUTH, Boolean.valueOf(false)).with(WEST, Boolean.valueOf(false)));
     }
 
-    @Override
-    public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return shape;
-    }
+    //public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+    //    return shape;
+    //}
 
-    @Override
-    public VoxelShape getRenderShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
-        return shape;
-    }
+    //@Override
+    //public VoxelShape getRenderShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+    //    return shape;
+    //}
 
-    @Override
-    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return shape;
-    }
+    //@Override
+    //public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+    //    return shape;
+    //}
 
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
@@ -74,6 +76,19 @@ public class TransportPipe extends Block{
         BlockState west = ibr.getBlockState(pos.west());
         return super.getStateForPlacement(context).with(UP, Boolean.valueOf(getConnectedState(up))).with(DOWN, Boolean.valueOf(getConnectedState(down)))
                 .with(NORTH, Boolean.valueOf(getConnectedState(north))).with(EAST, Boolean.valueOf(getConnectedState(east)))
+                .with(SOUTH, Boolean.valueOf(getConnectedState(south))).with(WEST, Boolean.valueOf(getConnectedState(west)));
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        BlockState up = worldIn.getBlockState(currentPos.up());
+        BlockState down = worldIn.getBlockState(currentPos.down());
+        BlockState north = worldIn.getBlockState(currentPos.north());
+        BlockState east = worldIn.getBlockState(currentPos.east());
+        BlockState south = worldIn.getBlockState(currentPos.south());
+        BlockState west = worldIn.getBlockState(currentPos.west());
+        return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos).with(UP, Boolean.valueOf(getConnectedState(up)))
+                .with(DOWN, Boolean.valueOf(getConnectedState(down))).with(NORTH, Boolean.valueOf(getConnectedState(north))).with(EAST, Boolean.valueOf(getConnectedState(east)))
                 .with(SOUTH, Boolean.valueOf(getConnectedState(south))).with(WEST, Boolean.valueOf(getConnectedState(west)));
     }
 
